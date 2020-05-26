b8bcdcef078cd8b58578bcad6a0924a53639b25b
mario <mario24jsp@gmail.com>
2020-05-26 23:35:16 +0100

    cookies are a go for now

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 	"strconv"
+	"strings"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+const (
+	coockieDelimiter = "|"
 )
 
 var tpl *template.Template
@@ -21,6 +29,18 @@ func main() {
 
 func index(w http.ResponseWriter, req *http.Request) {
 
+	sessionCookie := getUserSessionCookie(w, req)
+
+	counterCookie := getUserVisitCouterCookie(w, req)
+
+	newCoockieValues := appendValueCookie(sessionCookie, w)
+
+	fmt.Printf("Sesssion ID cookie is the following: %v and CounterCookie is the following: %v", sessionCookie.Value, counterCookie.Value)
+
+	tpl.ExecuteTemplate(w, "index.gohtml", newCoockieValues)
+}
+
+func getUserVisitCouterCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
 	ck, err := req.Cookie("visitCounter")
 
 	if err == http.ErrNoCookie {
@@ -38,5 +58,49 @@ func index(w http.ResponseWriter, req *http.Request) {
 
 	http.SetCookie(w, ck)
 
-	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+	return ck
+}
+
+func getUserSessionCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
+
+	ck, err := req.Cookie("sessionId")
+
+	if err == http.ErrNoCookie {
+		sID, _ := uuid.NewV4()
+		coockieExpireTime := 10
+
+		ck = &http.Cookie{
+			Name:   "sessionId",
+			Value:  sID.String(),
+			MaxAge: coockieExpireTime,
+		}
+
+		ck.Value += "|"
+		http.SetCookie(w, ck)
+	}
+
+	return ck
+}
+
+func appendValueCookie(ck *http.Cookie, w http.ResponseWriter) []string {
+
+	picNames := strings.Split(ck.Value, coockieDelimiter)
+
+	if strings.Contains(ck.Value, coockieDelimiter) {
+
+		if len(picNames) < 3 {
+			i := 0
+			for len(picNames) < 5 {
+				fmt.Println(len(picNames))
+				picName := "picNumber" + strconv.Itoa(len(picNames))
+				ck.Value += picName + "|"
+				picNames = append(picNames, picName)
+				i++
+			}
+
+			http.SetCookie(w, ck)
+		}
+	}
+
+	return picNames
 }
